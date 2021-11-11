e01ecf381446532bd23c53195c164334f5f00ef9
HW91 <63923099+HW91@users.noreply.github.com>
2021-11-11 13:38:30 -0500

    loading

--- script.js
+++ script.js
@@ -534,3 +534,10 @@ map.on("load", async () => {
     }
   }
 });
+
+$('#foo').ready(function () {
+    $('#loadingMessage').css('display', 'none');
+});
+$('#foo').load(function () {
+    $('#loadingMessage').css('display', 'none');
+});
