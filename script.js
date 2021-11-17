a846f8a9042d4d8d052289becbca20152dffe931
HW91 <63923099+HW91@users.noreply.github.com>
2021-11-16 21:54:20 -0500

    1st loader

--- script.js
+++ script.js
@@ -534,3 +534,13 @@ map.on("load", async () => {
       throw new Error(err);
     }
   }
+  
+  function hideLoader() {
+    $('#loading').hide();
+}
+
+$(window).ready(hideLoader);
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
+});
